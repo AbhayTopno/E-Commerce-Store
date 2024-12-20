4fca7154bb48c938a1eaf1a9cbc07538895f8a69
Abhay <abhaytopno999@gmail.com>
2024-12-21 01:46:12 +0530

    Addition of Category Page in frontend and backend

---
+++ backend/controllers/categoryController.js
@@ -0,0 +1,85 @@
+import Category from "../models/categoryModel.js";
+import asyncHandler from "../middlewares/asyncHandler.js";
+
+const createCategory = asyncHandler(async (req, res) => {
+  try {
+    const { name } = req.body;
+
+    if (!name) {
+      return res.status(400).json({ error: "Name is required" });
+    }
+
+    const existedCategory = await Category.findOne({ name: name.trim() });
+
+    if (existedCategory) {
+      return res.status(409).json({ error: "Category already exists" });
+    }
+
+    const category = await new Category({ name: name.trim() }).save();
+    res.status(201).json(category);
+  } catch (error) {
+    console.log(error);
+    return res
+      .status(500)
+      .json({ error: "An error occurred while creating the category" });
+  }
+});
+
+const updateCategory = asyncHandler(async (req, res) => {
+  try {
+    const { name } = req.body;
+    const { categoryId } = req.params;
+
+    const category = await Category.findOne({ _id: categoryId });
+
+    if (!category) {
+      return res.status(404).json({ error: "Category not found" });
+    }
+
+    category.name = name;
+
+    const updatedCategory = await category.save();
+    res.json(updatedCategory);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Server Error" });
+  }
+});
+
+const removeCategory = asyncHandler(async (req, res) => {
+  try {
+    const removed = await Category.findByIdAndDelete(req.params.categoryId);
+    res.json(removed);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Server Error" });
+  }
+});
+
+const listCategory = asyncHandler(async (req, res) => {
+  try {
+    const all = await Category.find({});
+    res.json(all);
+  } catch (error) {
+    console.log(error);
+    return res.status(400).json(error.message);
+  }
+});
+
+const readCategory = asyncHandler(async (req, res) => {
+  try {
+    const category = await Category.findOne({ _id: req.params.id });
+    res.json(category);
+  } catch (error) {
+    console.log(error);
+    return res.status(400).json(error.message);
+  }
+});
+
+export {
+  createCategory,
+  updateCategory,
+  removeCategory,
+  listCategory,
+  readCategory,
+};
